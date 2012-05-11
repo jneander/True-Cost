c492e4c1c16a738c66ef854b8b994e92ee0a76d7
Jeremy Neander <subjectego@gmail.com>
2012-05-11 02:34:17 -0500

    refactored: Calculation class

--- src/com/jneander/truecost/data/Calculation.java
+++ src/com/jneander/truecost/data/Calculation.java
@@ -1,95 +1,86 @@
 package com.jneander.truecost.data;
 
 public class Calculation {
-	private double balance, APR, payment;
-	private double totalCost, totalInterest;
-	private int duration;
+  private double balance, APR, payment;
+  private double totalCost, totalInterest, monthlyInterestRate;
+  private int duration;
 
-	public Calculation( double balance, double APR, double payment ) {
-		setBalance( balance );
-		setAPR( APR );
-		setPayment( payment );
-	}
+  public Calculation( double balance, double APR, double payment ) {
+    setBalance( balance );
+    setAPR( APR );
+    setPayment( payment );
+  }
 
-	public void setBalance( double balance ) {
-		this.balance = balance;
-	}
+  public void setBalance( double balance ) {
+    this.balance = balance;
+  }
 
-	public void setAPR( double APR ) {
-		this.APR = APR;
-	}
+  public void setAPR( double APR ) {
+    this.APR = APR;
+    monthlyInterestRate = APR / 1200.0;
+  }
 
-	public void setPayment( double payment ) {
-		this.payment = payment;
-	}
+  public void setPayment( double payment ) {
+    this.payment = payment;
+  }
 
-	public double getBalance() {
-		return this.balance;
-	}
+  public double getBalance() {
+    return this.balance;
+  }
 
-	public double getAPR() {
-		return this.APR;
-	}
+  public double getAPR() {
+    return this.APR;
+  }
 
-	public double getPayment() {
-		return this.payment;
-	}
+  public double getPayment() {
+    return this.payment;
+  }
 
-	public int getDuration() {
-		return this.duration;
-	}
+  public int getDuration() {
+    return this.duration;
+  }
 
-	/* Commit to the calculation */
-	public boolean calculate() {
-		// Ensure that usable values are provided
-		if ( Double.isNaN( balance ) || Double.isNaN( APR )
-				|| Double.isNaN( payment ) || paymentIsTooSmall() ) {
-			return false;
-		}
-		
-		// TODO: Protect against 'impossible payoff' scenario.
+  public boolean calculate() {
+    boolean returnSuccess = !(Double.isNaN( balance ) || Double.isNaN( APR )
+        || Double.isNaN( payment ) || paymentIsTooSmall());
 
-		// Initialize local storage.
-		double balanceRemaining = balance;
-		totalInterest = 0;
-		totalCost = 0;
-		duration = 0;
+    double monthlyInterestRate = APR / 1200.0;
+    double balanceRemaining = balance;
 
-		// Perform calculations until balance is reduced to target.
-		while ( balanceRemaining > 0 ) {
-			// If the balance is greater than the payment...
-			if ( balanceRemaining > payment ) {
-				// Apply the payment.
-				totalCost += payment;
-				balanceRemaining -= payment;
+    totalInterest = 0;
+    totalCost = 0;
+    duration = 0;
 
-				// TODO: Verify that no other interest functions apply.
-				
-				// Apply the interest.
-				totalInterest += balanceRemaining * (APR / 1200.0);
-				balanceRemaining *= 1 + (APR / 1200.0);
-			} else {
-				// Apply one final payment.
-				totalCost += balanceRemaining;
-				balanceRemaining = 0.0; // balance is now zero
-			}
-			
-			// Increment the duration.
-			duration++;
-		}
+    if ( returnSuccess )
+      while ( balanceRemaining > 0 ) {
+        if ( balanceRemaining > payment ) {
+          totalCost += payment;
+          balanceRemaining -= payment;
 
-		return true;
-	}
+          double currentInterest = balanceRemaining * monthlyInterestRate;
 
-	public double getInterest() {
-		return this.totalInterest;
-	}
-	
-	public double getCost() {
-		return this.totalCost;
-	}
-	
-	public boolean paymentIsTooSmall() {
-	  return (balance <= (balance - payment) * (APR / 1200.0));
-	}
+          totalInterest += currentInterest;
+          balanceRemaining += currentInterest;
+        } else {
+          totalCost += balanceRemaining;
+          balanceRemaining = 0.0;
+        }
+
+        duration++;
+      }
+
+    return returnSuccess;
+  }
+
+  public double getInterest() {
+    return this.totalInterest;
+  }
+
+  public double getCost() {
+    return this.totalCost;
+  }
+
+  public boolean paymentIsTooSmall() {
+    return (payment <= (balance - payment) * monthlyInterestRate);
+  }
 }
